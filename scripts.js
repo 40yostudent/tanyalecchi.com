5838014f53b241f59d6dc0375da45869b3dac29f
Sabino Paulicelli <sabino@paulicelli.com>
2018-09-15 21:49:40 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let menuButton = document.getElementById('menu-button');
+let navMenu = document.getElementsByTagName('nav')[0];
+
+// MENU BUTTON AND NAV:
+menuButton.addEventListener(
+    'click',
+    () => openMenu(menuButton, navMenu),
+    false
+);
+
+function openMenu(button, menu) {
+    if (!menu.getAttribute('style')) {
+        // button.children[0].classList.remove('fa-bars');
+        // button.children[0].classList.add('fa-chevron-up');
+
+        menu.style.transform = 'translateY(4.9rem)';
+    } else {
+        // button.children[0].classList.remove('fa-chevron-up');
+        // button.children[0].classList.add('fa-bars');
+
+        menu.removeAttribute('style');
+    }
+}
