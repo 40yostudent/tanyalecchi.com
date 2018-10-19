2e629f6770615fc8a5b7e27305f4d328acb1c211
Sabino Paulicelli <sabino@paulicelli.com>
2018-10-19 17:34:48 +0200

    about us page

--- about-us.php
+++ about-us.php
@@ -1,5 +1,133 @@
 <?php include("header.php"); ?>
 
-<main></main>
+<main>
+
+    <div class="banner" id="banner-about-us">
+        <h1>about us</h1>
+    </div>
+
+    <div id="intro">
+        <p>
+            Welcome!<br>Here at Broad Horizons we welcome referrals from professionals, adult self-referrals, parents and carers of children and young people. We offer a range of Psychotherapy, Therapeutic Interventions, Assessments, Clinical Supervision and Trainings all under one roof.<br>Broad Horizons is the only center in the region specialising in developmental and relational trauma for children and young people. We focus in working with complex issues such as developmental trauma, complex PTSD, dissociation and adaptive attachments in children and families where the child is looked after in care or in Post adoption.<br>However, this is by no means exclusive and self-referrals for adults or children are very much welcomed. all referrals are reviewed by the Clinical Lead to ensure that the appropriate therapist and skill set is matched to whatever the presenting needs. We are privileged at Broad Horizons to have a large number of experienced and highly qualified therapists enabling us to tailor made packages of support that meet the individuals need.<br>We pride ourselves on adopting a holistic family approach to our work with children and young people and are inclusive of multi-agency working and being proactive as part of the team around the child and family to provide the best possible outcomes for children, young people and families.
+        </p>
+        <p>
+            Broad Horizons is based in a rural part of Northamptonshire surrounded by fields and nature. The Broad Horizons house comprises of several therapy rooms and an idyllic garden where a number of small pets are homed. These pets are part of the Broad Horizons family and are adored by many of the clients and visitors.
+        </p>
+    </div>
+
+    <div id="claire-blurb">
+        <h2>claire harrison - breed</h2>
+        <img src="./images/pict-claire.jpg" alt="claire picture">
+        <p>
+            I first founded Broad Horizon in 2001, at the time as an individual practitioner. I am passionate about ensuring we move away from viewing children, young people or adults needing therapy as dysfunctional. At Broad Horizons we value individual’s strength and creativity when met with challenge, trauma or adversity. My motivation in creating a diverse therapeutic service was to ensure that children and families are not expected to fit into whatever services are available, but ensure that services including therapy, parent support, training etcetera are tailored to the individual child and family’s needs.
+        </p>
+        <p>
+            <em>As well as therapy and teaching and consultation Claire is sectary of the Europe Society for Trauma and Dissociation, and is a Board member of the British Association of Counselling and Psychotherapies Children, Young People and Families.</em>
+        </p>
+        <p>
+            As a UKCP and BACP Child and Adult Psychotherapist, Claire specialises in working with trauma and dissociation with children young people and adults. Alongside therapeutic practice she is a Senior Lecturer in Counselling for the University of Northampton. Claire has over 25 years’ experience working therapeutically in a range of setting, as a registered Gestalt Psychotherapist, Counsellor, CBT therapist, Play Therapist and Social worker. Claire lectures, researches and publishes both nationally and internationally in the field of trauma and dissociation. Claire also offers consultation and supervision to a range of Adoption and Fostering Agencies in the UK.
+        </p>
+    </div>
+
+    <div id="team">
+
+    <h2>our team</h2>
+    <p>Since 2001 we have grown into a well-respected Therapy Centre that specialises in working with trauma, dissociation and attachment issues.</p>
+
+    <div>
+        <img src="./images/squared-claire.jpg">
+        <div>
+            <h5>claire</h5>
+            <p>As a UKCP and BACP Child and Adult Psychotherapist, Claire specialises in working with trauma and dissociation with children, young people and adults.</p>
+        </div>
+    </div>
+
+    <div>
+        <img src="./images/squared-sarah.jpg">
+        <div>
+            <h5>sarah</h5>
+            <p>Clinical Lead for Broad Horizons team Sarah has worked within Social Care for over 20 years. She offers Video Interaction Guidance Supervision for VIG Guiders.</p>
+        </div>
+    </div>
+
+    <div>
+        <img src="./images/squared-generic.png">
+        <div>
+            <h5>audrey</h5>
+            <p>Audrey has been a counsellor and psychotherapist spanning 25 years. She can offer individual therapy for children, young people and adults as well as providing parent led CBT.</p>
+        </div>
+    </div>
+
+    <div>
+        <img src="./images/squared-amy.jpg">
+        <div>
+            <h5>amy</h5>
+            <p>Amy has been working therapeutically with children and young people for over 12 years. She offers individual therapy for children, young people and adults.</p>
+        </div>
+    </div>
+
+    <div>
+        <img src="./images/squared-lucy.jpg">
+        <div>
+            <h5>lucy</h5>
+            <p>
+                Lucy has worked with individuals whom present with trauma, dissociation, anxiety, depression, bereavement and loss, self-esteem, self-harm and suicidal idealisation.
+            </p>
+        </div>
+    </div>
+
+    <div>
+        <img src="./images/squared-sophie.jpg">
+        <div>
+            <h5>sophie</h5>
+            <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+            </p>     
+        </div>
+    </div>
+
+    <div>
+        <img src="./images/squared-erin.jpg">
+        <div>
+            <h5>erin</h5>
+            <p>
+                Within her therapy Erin works interactively, particularly enjoying play and creative activities. She is trauma and dissociation informed and will work therapeutically with this in mind.
+            </p>
+        </div>
+    </div>
+
+    <div>
+        <img src="./images/squared-luke.jpg">
+        <div>
+            <h5>luke</h5>
+            <p>
+                Luke is specialized in Gender and sexual identities - particularly trans and non-binary identities, relationship styles, eating disorders and self-harm.
+            </p>
+        </div>
+    </div>
+
+    <div>
+        <img src="./images/squared-hayley.jpg">
+        <div>
+            <h5>hayley</h5>
+            <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+            </p>
+        </div>
+    </div>
+
+    <div>
+        <img src="./images/squared-demelza.jpg">
+        <div>
+            <h5>demelza</h5>
+            <p>
+                Demelza is an assistant within our admin team. In this role, Demelza is the first points of contact between clients and Broad Horizons.
+            </p>
+        </div>
+    </div>
+
+    </div>
+
+</main>
 
 <?php include("footer.php"); ?>
