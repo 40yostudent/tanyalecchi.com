1c09238dc87c0e833d9cc407f808954678234312
Sabino Paulicelli <sabino@paulicelli.com>
2018-11-05 16:44:30 +0100

    comments erased

--- map.php
+++ map.php
@@ -18,11 +18,6 @@
 <?php include("header.php"); ?>
 
 <main>
-    <!-- <div style="color: black; width: 50%; margin: 25%; margin-top: 0; padding-top: 25%; text-align: center;">
-    <h1 style="color: black; text-align: center;">work in progress</h1>
-    <p>This page under construction,<br>we're sorry for the inconvenience.</p>
-    </div> -->
-
     <div class="banner" id="banner-map">
         <h1>how to find us</h1>
     </div>
